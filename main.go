32cf230b369c5a9b990f1a45a1585013b0120e89
jojohappy <sarahdj0917@gmail.com>
2018-05-11 13:51:23 +0800

    Fixed block by http server

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/jojohappy/luxun/pkg/controller"
 	luxunhttp "github.com/jojohappy/luxun/pkg/http"
@@ -15,12 +20,23 @@ var listenPort = flag.Int("port", 9280, "listen port")
 var prometheusEndpoint = flag.String("prometheus_endpoint", "/metrics", "Endpoint to expose Prometheus metrics on")
 
 func main() {
+	flag.Parse()
 	stream.Init()
 	mux := http.NewServeMux()
 	luxunhttp.RegisterHandler(mux, *prometheusEndpoint)
 
-	controller.Execute()
+	go controller.Execute()
 
-	server := fmt.Sprintf("%s:%d", *listenIp, *listenPort)
-	http.ListenAndServe(server, mux)
+	s := &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", *listenIp, *listenPort),
+		Handler: mux,
+	}
+	go log.Fatal(s.ListenAndServe())
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGTERM)
+	signal.Notify(sigterm, syscall.SIGINT)
+	<-sigterm
+	controller.Stop()
+	s.Shutdown(context.Background())
 }
